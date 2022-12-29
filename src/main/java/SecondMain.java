fa4585cb20a5736b85e713792d0546c864aa5c65
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2022-12-29 23:09:04 +0500

    3.1.1

---
+++ src/main/java/SecondMain.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SecondMain {
+    public static void main(String[] args) throws RuntimeException {
+        String host = "127.0.0.1";
+        int port = 8282;
+        try(Socket socket = new Socket(host,port)){
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            printWriter.println("Àðò¸ì");
+            String response = bufferedReader.readLine();
+            System.out.println(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
